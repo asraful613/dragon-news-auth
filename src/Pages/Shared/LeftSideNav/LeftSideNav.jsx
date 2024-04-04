c719cfb07ca4ddf1edf0f8a12b92112d6b728dd5
asraful613 <atifasraful530@gmail.com>
2024-04-04 11:10:05 +0100

    added a news category

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+    const [categories,setCategories]=useState([]);
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res=>res.json())
+        .then(data => setCategories(data))
+    },[])
     return (
-        <div>
-            <h2 className="text-2xl">Left Side</h2>
+        <div className="space-y-5">
+            <h2 className="text-2xl">All Categories</h2>
+            {
+                categories.map(category => <Link to={`/category/${category.id}`} className="block ml-4 text-xl font-semibold" key={categories.id}>{category.name}</Link>)
+            }
         </div>
     );
 };
